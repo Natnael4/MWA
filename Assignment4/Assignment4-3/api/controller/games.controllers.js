0382218099c7446e8ec88f03e406d92a24af17f7
Natnael4 <87682063+Natnael4@users.noreply.github.com>
2021-09-30 22:29:07 -0500

    Update games.controllers.js

--- Assignment4/Assignment4-3/api/controller/games.controllers.js
+++ Assignment4/Assignment4-3/api/controller/games.controllers.js
@@ -4,16 +4,20 @@ const dbConnection = require("../data/dbconnection.js");
 
 module.exports.gamesGetAll = function (req, res) {
     const db = dbConnection.get();
-    const collection = db.collection("games"); 
-    var offset = 0;
-    var count = 6;
-    if (req.query && req.query.offset) {
-        offset = parseInt(req.query.offset);
-    }
+    const collection = db.collection("games");
+
+       var count = 6;
+  
     if (req.query && req.query.count) {
-        count = parseInt(req.query.count);
+        newcount = parseInt(req.query.count);
+        if (newcount>9){
+           res.status(404).send("Game number exceeds 9 games");
+        }
+        else{
+            count = newcount;
+            }
     }
-    collection.find().skip(offset).limit(count).toArray(function (err, docs) {
+    collection.find().limit(count).toArray(function (err, docs) {
         console.log("Found games", docs);
         res.status(200).json(docs);
     });
