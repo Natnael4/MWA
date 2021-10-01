1cba91bd31bdc589ae212ff029fc883e7f350ab0
Natnael4 <87682063+Natnael4@users.noreply.github.com>
2021-09-30 22:00:49 -0500

    Update dbconnection.js

--- Assignment4/Assignment4-3/api/data/dbconnection.js
+++ Assignment4/Assignment4-3/api/data/dbconnection.js
@@ -1,19 +1,19 @@
 const MongoClient = require("mongodb").MongoClient; 
 const dbName = "newTestDB";
 const dburl = "mongodb://localhost:27017/" + dbName; 
-var_connection = null;
+var conn = null;
 var open = function () {
     MongoClient.connect(dburl, { useUnifiedTopology: true }, function (err, client) {
         if (err) {
             console.log("DB connection failed");
             return;
         }
-        _connection = client.db(dbName);
-        console.log("DB connection open", _connection);
+        conn = client.db(dbName);
+        console.log("DB connection open", conn);
     });
 };
 var get = function () {
-    return _connection;
+    return conn;
 };
 module.exports = {
     open: open, 
